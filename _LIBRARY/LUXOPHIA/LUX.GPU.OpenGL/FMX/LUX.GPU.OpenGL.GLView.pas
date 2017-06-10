60247c29ac750e68d94ccc10bc7939176f1eff6f
luxidea <contact@luxidea.net>
2017-06-10 19:33:28 +0900

    LUX.GPU.OpenGL.FMX を先読み。

--- _LIBRARY/LUXOPHIA/LUX.GPU.OpenGL/FMX/LUX.GPU.OpenGL.GLView.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.OpenGL/FMX/LUX.GPU.OpenGL.GLView.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
   Winapi.Windows, Winapi.OpenGL, FMX.Platform.Win,
-  LUX, LUX.GPU.OpenGL, LUX.GPU.OpenGL.Geometry;
+  LUX, LUX.GPU.OpenGL, LUX.GPU.OpenGL.FMX, LUX.GPU.OpenGL.Geometry;
 
 type
   TGLView = class(TFrame)
